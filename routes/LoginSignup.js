77fad4fa667cc2d127e1ae6be7e0a448cf56947e
sarvasva86 <165198643+sarvasva86@users.noreply.github.com>
2025-01-28 15:42:09 -0500

    Create LoginSignup.js

---
+++ routes/LoginSignup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LoginSignup() {
+  return (
+    <div>
+      <h1>Login or Sign Up</h1>
+      <p>Login and signup forms will go here.</p>
+    </div>
+  );
+}
+
+export default LoginSignup;
