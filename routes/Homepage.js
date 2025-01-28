e4e335989914e6967276cb108841ab0cdce58338
sarvasva86 <165198643+sarvasva86@users.noreply.github.com>
2025-01-28 15:39:49 -0500

    Create Homepage.js

---
+++ routes/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Homepage() {
+  return (
+    <div>
+      <h1>Welcome to Jobly</h1>
+      <p>This is the homepage.</p>
+    </div>
+  );
+}
+
+export default Homepage;
