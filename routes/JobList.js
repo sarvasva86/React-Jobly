ce3e0d0f7dacd4ff2443e62b6f799c49e1c9eff4
sarvasva86 <165198643+sarvasva86@users.noreply.github.com>
2025-01-28 15:41:35 -0500

    Create JobList.js

---
+++ routes/JobList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function JobsList() {
+  return (
+    <div>
+      <h1>All Jobs</h1>
+      <p>List of jobs will appear here.</p>
+    </div>
+  );
+}
+
+export default JobsList;
