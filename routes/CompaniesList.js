251ce18d019e17739222bc38f69eb10f97f73667
sarvasva86 <165198643+sarvasva86@users.noreply.github.com>
2025-01-28 15:40:21 -0500

    Create CompaniesList.js

---
+++ routes/CompaniesList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CompaniesList() {
+  return (
+    <div>
+      <h1>All Companies</h1>
+      <p>List of all companies will appear here.</p>
+    </div>
+  );
+}
+
+export default CompaniesList;
